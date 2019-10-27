8c998c05065a17d0320b816cb4825ea72e73db41
Fabbi <github@derfabbi.de>
2019-10-27 14:50:38 +0100

    made qansitextedit msvc compatible

--- src/widgets/qansitextedit.cpp
+++ src/widgets/qansitextedit.cpp
@@ -100,66 +100,67 @@ void QTE::insertIntoDocument(QTextDocument* document, const QString& text)
 void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
                               QTextCharFormat& format, QTextCharFormat const & defaultFormat)
 {
-    switch (attr) {
-    case 0 : // Normal/Default (reset all attrs)
-      {format = defaultFormat; break;}
-    case 1 : // Bold/Bright (bold or increased intensity)
-      {format.setFontWeight(QFont::Bold); break;}
-    case 2 : // Dim/Faint (decreased intensity)
-      {format.setFontWeight(QFont::Light); break;}
-    case 3 : // Italicized (italic on)
-      {format.setFontItalic(true); break;}
-    case 4 : {// Underscore (single underlined)
+    if (attr == 0) // Normal/Default (reset all attrs)
+      {format = defaultFormat; }
+    else if (attr == 1) // Bold/Bright (bold or increased intensity)
+      {format.setFontWeight(QFont::Bold); }
+    else if (attr == 2) // Dim/Faint (decreased intensity)
+      {format.setFontWeight(QFont::Light); }
+    else if (attr == 3) // Italicized (italic on)
+      {format.setFontItalic(true); }
+    else if (attr == 4) {// Underscore (single underlined)
       format.setUnderlineStyle(QTextCharFormat::SingleUnderline);
-      format.setFontUnderline(true); break;}
-    case 5 : // Blink (slow, appears as Bold)
-      {format.setFontWeight(QFont::Bold); break;}
-    case 6 : // Blink (rapid, appears as very Bold)
-      {format.setFontWeight(QFont::Black); break;}
-    case 7 : {// Reverse/Inverse (swap foreground and background)
+      format.setFontUnderline(true); }
+    else if (attr == 5) // Blink (slow, appears as Bold)
+      {format.setFontWeight(QFont::Bold); }
+    else if (attr == 6) // Blink (rapid, appears as very Bold)
+      {format.setFontWeight(QFont::Black); }
+    else if (attr == 7) {// Reverse/Inverse (swap foreground and background)
       QBrush foregroundBrush = format.foreground();
       format.setForeground(format.background());
-      format.setBackground(foregroundBrush); break;}
-    case 8 : // Concealed/Hidden/Invisible (usefull for passwords)
-      {format.setForeground(format.background()); break;}
-    case 9 : // Crossed-out characters
-      {format.setFontStrikeOut(true); break;}
-    case 10 : // Primary (default) font
-      {format.setFont(defaultFormat.font()); break;}
-    case 11 ... 19 : {
+      format.setBackground(foregroundBrush); }
+    else if (attr == 8) // Concealed/Hidden/Invisible (usefull for passwords)
+      {format.setForeground(format.background()); }
+    else if (attr == 9) // Crossed-out characters
+      {format.setFontStrikeOut(true); }
+    else if (attr == 10) // Primary (default) font
+      {format.setFont(defaultFormat.font()); }
+    else if (attr >= 11 && attr <= 19) {
       QFontDatabase fontDatabase;
       QString fontFamily = format.fontFamily();
       QStringList fontStyles = fontDatabase.styles(fontFamily);
       int fontStyleIndex = attr - 11;
       if (fontStyleIndex < fontStyles.length())
-        format.setFont(fontDatabase.font(fontFamily, fontStyles.at(fontStyleIndex), format.font().pointSize()));
-      break;}
-    case 20 : // Fraktur (unsupported)
-      {break;}
-    case 21 : // Set Bold off
-      {format.setFontWeight(QFont::Normal); break;}
-    case 22 : // Set Dim off
-      {format.setFontWeight(QFont::Normal); break;}
-    case 23 : // Unset italic and unset fraktur
-      {format.setFontItalic(false); break;}
-    case 24 : // Unset underlining
+        format.setFont(fontDatabase.font(fontFamily,
+          fontStyles.at(fontStyleIndex),
+          format.font().pointSize()));
+      }
+    else if (attr == 20) // Fraktur (unsupported)
+      {}
+    else if (attr == 21) // Set Bold off
+      {format.setFontWeight(QFont::Normal); }
+    else if (attr == 22) // Set Dim off
+      {format.setFontWeight(QFont::Normal); }
+    else if (attr == 23) // Unset italic and unset fraktur
+      {format.setFontItalic(false); }
+    else if (attr == 24) // Unset underlining
       {format.setUnderlineStyle(QTextCharFormat::NoUnderline);
-      format.setFontUnderline(false); break;}
-    case 25 : // Unset Blink/Bold
-      {format.setFontWeight(QFont::Normal); break;}
-    case 26 : // Reserved
-      {break;}
-    case 27 : // Positive (non-inverted)
+      format.setFontUnderline(false); }
+    else if (attr == 25) // Unset Blink/Bold
+      {format.setFontWeight(QFont::Normal); }
+    else if (attr == 26) // Reserved
+      {}
+    else if (attr == 27) // Positive (non-inverted)
       {QBrush backgroundBrush = format.background();
       format.setBackground(format.foreground());
-      format.setForeground(backgroundBrush); break;}
-    case 28 :
+      format.setForeground(backgroundBrush); }
+    else if (attr == 28)
       {format.setForeground(defaultFormat.foreground());
-      format.setBackground(defaultFormat.background()); break;}
-    case 29 :
+      format.setBackground(defaultFormat.background()); }
+    else if (attr == 29)
       {format.setUnderlineStyle(QTextCharFormat::NoUnderline);
-      format.setFontUnderline(false); break;}
-    case 30 ... 37 : {
+      format.setFontUnderline(false); }
+    else if (attr >= 30 && attr <= 37) {
       int colorIndex = attr - 30;
       QColor color;
       if (QFont::Normal < format.fontWeight()) {
@@ -168,18 +169,15 @@ void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
         color = colors[colorIndex];
         }
       format.setForeground(color);
-      break;
     }
-    case 39 :
-      {format.setForeground(defaultFormat.foreground()); break;}
-    case 40 ... 47 : {
+    else if (attr == 39)
+      {format.setForeground(defaultFormat.foreground()); }
+    else if (attr >= 40 && attr <= 47) {
       int colorIndex = attr - 40;
       QColor color = colors[colorIndex];
       format.setBackground(color);
-      break;
     }
-    case 38 :
-    case 48 : {
+    else if (attr == 38 || attr == 48) {
       if (it.hasNext()) {
         bool ok = false;
         int selector = it.next().toInt(&ok);
@@ -203,14 +201,13 @@ void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
           if (!it.hasNext()) {break;}
           int index = it.next().toInt(&ok);
           Q_ASSERT(ok);
-          switch (index) {
-          case 0x00 ... 0x07 : { // 0x00-0x07:  standard colors (as in ESC [ 40..47 m)
+          if (index >= 0x00 && index <= 0x07) { // 0x00-0x07:  standard colors (as in ESC [ 40..47 m)
             return parseEscapeSequence(index - 0x00 + ((attr==38)?30:40), it, format, defaultFormat);
           }
-          case 0x08 ... 0x0F : { // 0x08-0x0F:  high intensity colors (as in ESC [ 100..107 m)
+          else if (index >= 0x08 && index <= 0x0F ) { // 0x08-0x0F:  high intensity colors (as in ESC [ 100..107 m)
             return parseEscapeSequence(index - 0x08 + ((attr==38)?90:100), it, format, defaultFormat);
           }
-          case 0x10 ... 0xE7 : { // 0x10-0xE7:  6*6*6=216 colors: 16 + 36*r + 6*g + b (0≤r,g,b≤5)
+          else if (index >= 0x10 && index <= 0xE7 ) { // 0x10-0xE7:  6*6*6=216 colors: 16 + 36*r + 6*g + b (0≤r,g,b≤5)
             index -= 0x10;
             int red = index % 6; index /= 6;
             int green = index % 6; index /= 6;
@@ -219,11 +216,10 @@ void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
             color.setRgb(red, green, blue);
             break;
           }
-          case 0xE8 ... 0xFF : { // 0xE8-0xFF:  grayscale from black to white in 24 steps
+          else if (index >= 0xE8 && index <= 0xFF ) { // 0xE8-0xFF:  grayscale from black to white in 24 steps
             qreal intensity = qreal(index - 0xE8) / (0xFF - 0xE8);
             color.setRgbF(intensity, intensity, intensity);
             break;
-          }
           }
           if (attr==38)
             format.setForeground(color);
@@ -234,12 +230,11 @@ void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
         default : {break;}
         }
       }
-      break;
     }
-    case 49 :
-      {format.setBackground(defaultFormat.background()); break;}
-    case 90 ... 97 :
-    case 100 ... 107 : {
+    else if (attr == 49)
+      {format.setBackground(defaultFormat.background());}
+    else if ((attr >= 90 && attr <= 97)
+             || (attr >= 100 && attr <= 107)) {
       int colorIndex = attr - 100;
       QColor color = colors[colorIndex];
 
@@ -250,11 +245,6 @@ void QTE::parseEscapeSequence(int attr, QListIterator<QString>& it,
         format.setForeground(color);
       else
         format.setBackground(color);
-      break;
-    }
-    default : {
-      break;
-    }
     }
   }
 
